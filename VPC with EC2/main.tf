00adf07e76277e0ced1a89d1abbcb5c7bce631b5
vinodguttula11467-cpu <vinodguttula11467@gmail.com>
2026-09-11 18:54:59 +0530

    Create main.tf

---
+++ VPC with EC2/main.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+resource "aws_instance" "test" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name     = var.key_name
+
+  tags = {
+    Name = var.instance_name
+  }
+}
