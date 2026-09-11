b696c806fa5fca3d698a35788ac97f90c5d2c2f0
vinodguttula11467-cpu <vinodguttula11467@gmail.com>
2026-09-11 18:57:56 +0530

    Add Terraform variables for VPC and EC2 configuration
    It contains variables

---
+++ VPC with EC2/variable.tf
@@ -0,0 +1,37 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.10.0.0/16"
+}
+
+variable "vpc_name" {
+  description = "VPC name"
+  type        = string
+  default     = "terraform-learning-vpc"
+}
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0b6d9d3d33ba97d99" # Replace with your desired AMI ID
+}
+variable "instance_type" {
+  description = "Instance type for the EC2 instance"
+  type        = string
+  default     = "t3.small" # Replace with your desired instance type
+}
+variable "instance_name" {
+  description = "Name tag for the EC2 instance"
+  type        = string
+  default     = "Terraform-Managed-EC2"
+}
+variable "key_name" {
+  description = "Key pair name for the EC2 instance"
+  type        = string
+  default     = "terraform" # Replace with your actual key pair name
+}
